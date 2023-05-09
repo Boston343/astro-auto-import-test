9ec84218c7c47c47a1ebd924e537a9248b547f84
Boston343 <branden.austin1@gmail.com>
2023-05-09 19:10:39 -0400

    This breaks. component is not imported

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import AutoImport from "astro-auto-import";
+// import HeaderLink from '../../components/HeaderLink.astro';
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    AutoImport({
+      imports: ["../../components/HeaderLink.astro"],
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
